cab969b0be7c1c9a71224e3e945195def7a37a4c
Stéphane Lepin <stephane.lepin@gmail.com>
2024-06-29 03:35:38 +0200

    env variables support

--- liquidsoap/run.sh
+++ liquidsoap/run.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/with-contenv bashio
 
+if bashio::config.has_value 'environment'; then
+    while read -r env_variable; do
+        export "$env_variable" \
+            || bashio::exit.nok "Failed to export $env_variable"
+    done <<< "$(bashio::config 'environment')"
+fi
+
 echo "yt-dlp version: $(yt-dlp --version)"
 
 liquidsoap /run.liq
